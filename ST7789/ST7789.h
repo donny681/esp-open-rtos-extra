5400a291d05673ebf4a8b588bb3e3e2e2a6bbe3f
aithinker <divter@qq.com>
2018-04-19 10:21:10 +0800

    change st7789 and add esp-cjson

--- ST7789/ST7789.h
+++ ST7789/ST7789.h
@@ -1,5 +1,11 @@
 #ifndef __ST7789_H
 #define __ST7789_H
+/*
+ * Note the setting of the direction and screen height and width,
+ * otherwise an error will occur
+ * The default direction is portrait,width:240,height:320
+ *
+ * */
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -13,11 +19,21 @@
 
 #define SPI_BUS 1   //spi bus use user.(0:system,1:user)
 
+#define LANDSCAPE	0		//hengping
+#define PORTRAIT	1		//shuping
+
+
 #define CMD_MODE 0  //command
 #define DATA_MODE 1 //data
 
-#define WHITE 0xffff
-#define BLACK 0x0000
+#define WHITE 	0xffff
+#define BLACK 	0x0000
+#define RED		0xf800
+#define GREEN	0x07E0
+#define BLUE	0x001f
+
+extern uint16_t BACK_COLOR;
+extern uint16_t FRONT_COLOR;
 
 #define LCD_delay(x) vTaskDelay((x)/portTICK_PERIOD_MS)
 
@@ -26,6 +42,7 @@ void writedata(uint8_t data);  //write data to lcd
 void writedata16(uint16_t data);//write 16 bit data to lcd
 void LCD_GPIO_Init(void);           //init GPIO
 void LCD_Init(void);            //init lcd
+void LCD_SetDir(uint8_t dir);
 void LCD_SetWindow(uint16_t xs,uint16_t ys,uint16_t xe,uint16_t ye);    //set display windows
 void LCD_ClrScreen(uint16_t color);       //clear screen
 void LCD_Fill(uint16_t xState,uint16_t yState,uint16_t xEnd,uint16_t yEnd,uint16_t color);  //fill windows with color
